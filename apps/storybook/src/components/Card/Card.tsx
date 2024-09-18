fa1a5ae277c2f91254be5b19941612b2535bf76b
Nam Vo <nam.vh@carehealth.io>
2024-09-19 03:25:51 +0700

    feat: add Foundations Typography

---
+++ apps/storybook/src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+
+import styles from "./Card.module.css";
+
+interface IProps {
+  lead: string;
+  title: string;
+  content: string;
+  leading?: ReactNode;
+}
+
+const Card = ({ lead, title, content, leading }: IProps) => {
+  return (
+    <div>
+      <div className={styles.lead}>{lead}</div>
+      <div className={styles.container}>
+        {leading}
+        <div className={styles.info}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.content}>{content}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- apps/storybook/src/components/DesignSystemPalette/DesignSystemPalette.tsx
+++ apps/storybook/src/components/DesignSystemPalette/DesignSystemPalette.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Card from "../Card";
+
 import styles from "./DesignSystemPalette.module.css";
 
 interface IProps {
@@ -13,24 +15,22 @@ const DesignSystemPalette = ({ name, colors }: IProps) => {
       {Object.keys(colors)
         .sort((a, b) => parseInt(a) - parseInt(b))
         .map((color) => (
-          <div key={`${name}-${color}`} className={styles.palette}>
-            <div className={styles.shade}>{color}</div>
-            <div className={styles.content}>
-              <div
-                className={styles.preview}
-                style={{
-                  backgroundColor: colors[color],
-                }}
-              >
-                &nbsp;
-              </div>
-              <div className={styles.info}>
-                <div className={styles.title}>Color</div>
-                <div className={styles.colorHex}>
-                  {colors[color].toUpperCase()}
+          <div key={`${name}-${color}`}>
+            <Card
+              lead={color}
+              title="Color"
+              content={colors[color].toUpperCase()}
+              leading={
+                <div
+                  className={styles.preview}
+                  style={{
+                    backgroundColor: colors[color],
+                  }}
+                >
+                  &nbsp;
                 </div>
-              </div>
-            </div>
+              }
+            />
           </div>
         ))}
     </div>
